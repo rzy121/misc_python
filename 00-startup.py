2851a3d893dea57dc5adfad854a10637cc47872f
rzy121 <38796582+rzy121@users.noreply.github.com>
2025-04-09 21:12:21 -0700

    corrected the order of plot style setting

--- 00-startup.py
+++ 00-startup.py
@@ -11,16 +11,20 @@ pd.set_option('display.max_columns', None)
 # Visualization
 import matplotlib.pyplot as plt
 import seaborn as sns
-plt.style.use('bmh')
-sns.set(style='darkgrid')
+
+
+sns.set_theme(style='darkgrid')
+plt.style.use('fivethirtyeight')
+
+# Auto-enable inline plotting
 
 try:
     from IPython import get_ipython
     ipython = get_ipython()
     if ipython:
+        ipython.run_line_magic('matplotlib', 'inline')
         ipython.run_line_magic('load_ext', 'autoreload')
         ipython.run_line_magic('autoreload', '2')
-        ipython.run_line_magic('matplotlib', 'inline')
 except Exception as e:
     pass  # You can log this if needed
 
